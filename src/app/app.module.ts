6517e4a84a5b78a0a15b334ee69a6baf4b8f4275
Daniel Guerra <unknown@example.com>
2018-09-27 09:28:43 +0200

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutes } from './app.routing';
+import { BeersModule } from './beers/beers.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes),
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    BeersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
